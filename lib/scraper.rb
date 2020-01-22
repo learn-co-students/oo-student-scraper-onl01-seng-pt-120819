50c0eb0a5bef0d487083f8f5b27ada350fdab386
Amanda Rolon <abrolon87@gmail.com>
2020-01-22 17:53:25 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,22 +4,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    source_page = Nokogiri::HTML(open(index_url))
-    scraped_students = []
+    source_page = Nokogiri::HTML(open(index_url)) # Where are you getting your info from?
+    scraped_students = [] # set empty array to push acquired information into
 
-    source_page.css("div.roster-cards-container").each do |roster|
-      roster.css(".student_card a").each do |student|
+    source_page.css("div.roster-cards-container").each do |roster| #iterate roster
+      roster.css(".student-card a").each do |student| # go in one level and iterate to gather information about students
         #assign scraped details to variables
         student_name = student.css(".student-name").text
         student_location = student.css(".student-location").text
         student_profile_url = "#{student.attr('href')}" #<a href="ryan-johnson.html">
-        scraped_students << {name: student_name, location: student_location, profile: student_profile_url} #each hash is one student
+        # set up hash with keys and variables as the values to be pushed into array; each hash is a different student
+        scraped_students << {name: student_name, location: student_location, profile_url: student_profile_url} #each hash is one student
 
       end
     end
     scraped_students
   end
-  binding.pry
+
   def self.scrape_profile_page(profile_url)
 
   end
