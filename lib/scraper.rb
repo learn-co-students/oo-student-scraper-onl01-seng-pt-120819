4bc3f62a142e7f908084813649d5e91915e90baa
Swishyfishie <pclandreiuk@gmail.com>
2020-01-28 18:50:33 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,53 +7,59 @@ class Scraper
     html = open(index_url)
     doc = Nokogiri::HTML(html)
     hashyhash = []
-    student_names = doc.css(".student-card a .card-text-container .student-name").text
-    student_location = doc.css(".student-card a .card-text-container .student-location").text
-    # student_urls = 
     x = doc.css(".student-card a")
 
-
-
-
-    # doc.css(".student-card a .card-text-container .student-name").each do |namez|
-    #   hashyhash << { ':name' => namez.text }
-    # end
-
-    # doc.css(".student-card a .card-text-container .student-location").each do |loc|
-    #   hashyhash << {':location' => loc.text }
-    # end
-
-    # doc.css('.student-card a').each do |link|
-    #    hashyhash << {':profile_url' => link['href']}
-    # end
-x.each do |x|
-  hashyhash << { :name =>"#{x.css('.student-name').text}", :location => "#{x.css('.student-location').text}", :profile_url => "#{x.first[1]}"}
-end
-
-    # binding.pry
-    pp hashyhash
+    x.each do |y|
+      hashyhash << { :name =>"#{y.css('.student-name').text}", :location => "#{y.css('.student-location').text}", :profile_url => "#{y.attr('href')}"}
+      # binding.pry
+    end
+    
+    hashyhash
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+    x = doc.css('.profile .vitals-container .social-icon-container a')
+    # twitter = x[0].attr('href')
+    # linkedin = x[1].attr('href')
+    # github = x[2].attr('href')
+    # blog = x[3].attr('href')
+
+
+    # hashyhash = {
+    #   :twitter => twitter ? twitter : nil,
+    #   :linkedin => linkedin ? linkedin : nil,
+    #   :github => github ? github : nil,
+    #   :blog => blog ? blog : nil,
+    #   :profile_quote => doc.css('.profile .vitals-text-container .profile-quote').text,
+    #   :bio => doc.css('.profile .details-container .description-holder p').text
+    # }
+
+    hashyhash = {
+    }
+    doc.css('.profile .vitals-container .social-icon-container').each do |y|
+      if y.css('a').attr('href').value.include?('twitter')
+        hashyhash[:twitter] = y.css('a').attr('href').value
+      elsif y.css('a').attr('href').value.include?('linkedin')
+        hashyhash[:linkedin] = y.css('a').attr('href').value 
+      elsif y.css('a').attr('href').value.include?('github')
+        hashyhash[:github] = y.css('a').attr('href').value 
+      elsif y.css('a').attr('href').value.include?('rss')
+        hashyhash[:blog] = y.css('a').attr('href').value
+      end
+      # binding.pry
+    end 
+pp hashyhash
+    # x.each do |y|
+    #   # puts y.attr('href')
+    #   hashyhash[key] = y.attr('href')
+    #   # binding.pry
+    # end
+    # binding.pry
+    # x.attr('href').value == twitter
   end
 
 end
-=begin
-stn.each do |names|
-  puts names.text.strip
-end -------------- this is a loop that puts all the names
-
-stn.each do |names|
-  puts names
-end
-
-{
-  name
-  location
-  profile_url
-}
 
 
-
-=end
