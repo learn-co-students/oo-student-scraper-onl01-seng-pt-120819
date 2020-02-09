7ce0ed7d289dd3e5cec8cffe85aa3a755af32c7e
asif-kamal <asif.kamal1489@gmail.com>
2020-02-08 21:32:17 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,30 +1,34 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
 
 
-doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+
 
 class Scraper
 
   #attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
-    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html/")
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
     doc = Nokogiri::HTML(html)
+#binding.pry
+    students = []
 
-    students = {}
-
-    doc.css("div.roster-cards-container").each do |card|
-     students[card] = {
-      :name => project.css("#ryan-johnson-card > a > div.card-text-container > h4").text,
-      :location => project.css("#ryan-johnson-card > a > div.card-text-container > p").text,
-      :profile_url => project.css("#ryan-johnson-card > a").text.gsub("%","").to_i
+    doc.css(".student-card").each do |student|
+      id = student.css("h4").text
+      students[id.to_sym] = {
+      :name => student.css("h4").text,
+      :location => student.css("p").text,
+      :profile_url => student.css("a").attribute('href').value
+      #binding.pry
     }
+
   end
 
   # return the projects hash
-  projects
+   students
   end
 
   def self.scrape_profile_page(profile_url)
